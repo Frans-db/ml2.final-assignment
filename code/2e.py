664e9e1a53d2ee54ed6f12e9139af69674e9e63f
Frans de Boer <frans-de-boer@hotmail.com>
2022-06-29 15:58:55 +0200

    draft for 2e

---
+++ code/2e.py
@@ -0,0 +1,11 @@
+from math import log2
+
+
+dimensionalities = [1, 2, 3, 4, 5, 6, 7, 8, 1024, 2**100]
+
+for d in dimensionalities:
+    best = -1
+    for N in range(1_000):
+        if 2*N*d >= 2**N:
+            best = N
+    print(d, best, 2* (log2(d) + 1))
